56f891f113316364455d2ade77ab06c6e5a89f7f
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2020-12-21 15:10:40 -0800

    manufacturer row slice

--- src/containers/manufacturers.js
+++ src/containers/manufacturers.js
@@ -6,7 +6,20 @@ export function ManufacturersContainer({ path, categories }) {
     return (
         <>
             <Manufacturer.Row>
-                {categories.map((category, index) => (
+                {categories.slice(0, 5).map((category, index) => (
+                    <>
+                        <Manufacturer key={index} src={category.slug} to={`${path}/${category.id}`}>
+                            <Manufacturer.Title>{category.name}</Manufacturer.Title>
+                            <Manufacturer.DescriptionContainer>
+                                <Manufacturer.Description>{category.description}</Manufacturer.Description>
+                            </Manufacturer.DescriptionContainer>
+                            <Manufacturer.LogoPlacement src={category.slug}></Manufacturer.LogoPlacement>
+                        </Manufacturer>
+                    </>
+                ))}
+            </Manufacturer.Row>
+            <Manufacturer.Row>
+                {categories.slice(5, ).map((category, index) => (
                     <>
                         <Manufacturer key={index} src={category.slug} to={`${path}/${category.id}`}>
                             <Manufacturer.Title>{category.name}</Manufacturer.Title>
