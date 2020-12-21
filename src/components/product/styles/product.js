cba53de6778de3adc93c61e61264cc18a8d57430
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2020-12-21 15:05:19 -0800

    product image add and fix

--- src/components/product/styles/product.js
+++ src/components/product/styles/product.js
@@ -33,8 +33,8 @@ export const Picture = styled.div`
     overflow: visible;
 
     img {
-        width: 200px;
-        height: auto;
+        width: auto;
+        height: 200px;
         filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
     }
 `;
