eb0678993f47bd612b4425a88cd8d48ac35c50ad
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2020-12-21 16:52:40 -0800

    product slice

--- src/containers/products.js
+++ src/containers/products.js
@@ -2,26 +2,43 @@ import React from 'react';
 import { Product } from '../components';
 
 export function ProductsContainer({ products, match }) {
-
+  
     const {
         params: { manufacturerId }
     } = match;
+    
+    const productFilter = products.filter((product) => product.categories[0].id === manufacturerId)
+    
+    console.log(productFilter);
 
     return (
         <>
             <Product.Row>
                 {
-                    products.slice(0, 5).map((product, index) => {
-                        return product.categories[0].id === manufacturerId &&
-                            <Product key={index}>
-                                <Product.Picture>
-                                    <img src={product.media.source} alt="Product Display" />
-                                </Product.Picture>
-                                <Product.Title>{product.name}</Product.Title>
-                                <Product.Description>{product.description.slice(0, -4).substring(3, )}</Product.Description>
-                                <Product.CartPrice>{product.price.formatted_with_symbol}</Product.CartPrice>
-                            </Product>
-                    })
+                    productFilter.slice(0, 5).map((product, index) => (
+                        <Product key={index}>
+                            <Product.Picture>
+                                <img src={product.media.source} alt="Product Display" />
+                            </Product.Picture>
+                            <Product.Title>{product.name}</Product.Title>
+                            <Product.Description>{product.description.slice(0, -4).substring(3, )}</Product.Description>
+                            <Product.CartPrice>{product.price.formatted_with_symbol}</Product.CartPrice>
+                        </Product>
+                    ))
+                }
+            </Product.Row>
+            <Product.Row>
+                {
+                    productFilter.slice(5, ).map((product, index) => (
+                        <Product key={index}>
+                            <Product.Picture>
+                                <img src={product.media.source} alt="Product Display" />
+                            </Product.Picture>
+                            <Product.Title>{product.name}</Product.Title>
+                            <Product.Description>{product.description.slice(0, -4).substring(3, )}</Product.Description>
+                            <Product.CartPrice>{product.price.formatted_with_symbol}</Product.CartPrice>
+                        </Product>
+                    ))
                 }
             </Product.Row>
         </>
