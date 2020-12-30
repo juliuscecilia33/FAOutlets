5c756b883ce463aa4f241deba716937d895c38fa
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2020-12-29 17:44:10 -0800

    payment form handle submit

--- src/containers/paymentform.js
+++ src/containers/paymentform.js
@@ -6,3 +6,43 @@ import { loadStripe } from '@stripe/stripe-js';
 import { ReviewContainer } from './review';
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+
+export function PaymentFormContainer({ checkoutToken, nextStep, backStep, shippingData, onCaptureCheckout, timeout }) {
+    const handleSubmit = async (event, elements, stripe) => {
+        event.preventDefault();
+    
+        if (!stripe || !elements) return;
+    
+        const cardElement = elements.getElement(CardElement);
+    
+        const { error, paymentMethod } = await stripe.createPaymentMethod({ type: 'card', card: cardElement });
+    
+        if (error) {
+          console.log('[error]', error);
+        } else {
+                const orderData = {
+                    line_items: checkoutToken.live.line_items,
+                    customer: { firstname: shippingData.firstName, lastname: shippingData.lastName, email: shippingData.email },
+                    shipping: { name: 'International', street: shippingData.address1, town_city: shippingData.city, county_state: shippingData.shippingSubdivision, postal_zip_code: shippingData.zip, country: shippingData.shippingCountry },
+                    fulfillment: { shipping_method: shippingData.shippingOption },
+                    payment: {
+                        gateway: 'stripe',
+                        stripe: {
+                        payment_method_id: paymentMethod.id,
+                    },
+                },
+            };
+
+            onCaptureCheckout(checkoutToken.id, orderData);
+
+            timeout();
+
+            nextStep();
+        }
+    }
+
+    return (
+        <>
+        </>
+    )
+}
