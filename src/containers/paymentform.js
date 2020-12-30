eb47954fa0d6b4d8592be4015ac79b0bdcf60d29
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2020-12-29 17:24:57 -0800

    stripe key

--- src/containers/paymentform.js
+++ src/containers/paymentform.js
@@ -3,4 +3,6 @@ import { Typography, Button, Divider } from '@material-ui/core';
 import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 
-import Review from './Review';
+import { ReviewContainer } from './review';
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
